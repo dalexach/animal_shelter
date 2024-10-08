f1e98b0d7ada48e2a31ebdf09cec19b7055cfeff
dalexach <dalexach@gmail.com>
2024-10-07 22:39:36 -0500

    Correction on models

--- backend/shelter/models.py
+++ backend/shelter/models.py
@@ -4,6 +4,22 @@ from django.contrib.auth.models import AbstractUser
 class Usuario(AbstractUser):
     nombre_completo = models.CharField(max_length=100)
     numero_celular = models.CharField(max_length=20)
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='groups',
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        related_name='usuario_set',  # Cambiado de 'user_set' a 'usuario_set'
+        related_query_name='usuario',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='user permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_name='usuario_set',  # Cambiado de 'user_set' a 'usuario_set'
+        related_query_name='usuario',
+    )
 
 class Cuidador(Usuario):
     class Meta:
