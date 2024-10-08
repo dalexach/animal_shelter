97dbcf9739dad891f0087a35c783fec316499932
dalexach <dalexach@gmail.com>
2024-10-07 22:46:56 -0500

    Adding swager

--- backend/animal_shelter/settings.py
+++ backend/animal_shelter/settings.py
@@ -22,7 +22,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shelter',  # Asegúrate de que tu app 'shelter' esté incluida aquí
+    'drf_yasg',
+    'shelter',
     'rest_framework',
     'corsheaders',
 ]
